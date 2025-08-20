bc520f6bb9534654019135bf4aa7c1f4f164d2bf
lktec-io <lktecio@gmail.com>
2025-08-20 14:37:33 +0300

    --

--- database/event.js
+++ database/event.js
@@ -1,25 +1,24 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
+
 
 const sequelize = new Sequelize({
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: "localhost",
+  dialect: "mysql",
+  username: "root",
+  password: "Leonard1234#1234",
+  database: "events",
   benchmark: true,
   dialectModule: require("mysql2"),
   logging: false,
 });
-
-
+ 
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log("✅ Database connection has been established successfully.");
-    await sequelize.sync({ alter: true }); // auto update tables if changed
+    console.log("Database connection has been established successfully.");
+    await sequelize.sync({ alter: true });
   } catch (error) {
-    console.error("❌ Unable to connect to the database:", error);
+    console.error("Unable to connect to the database:", error);
   }
 })();
 
