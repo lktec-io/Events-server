e5a2d000fdeb72232ed86cbfe627aad827e440e5
lktec-io <lktecio@gmail.com>
2025-08-20 12:57:29 +0300

    database-changed

--- database/event.js
+++ database/event.js
@@ -1,24 +1,24 @@
 const { Sequelize } = require("sequelize");
-
+require("dotenv").config();
 
 const sequelize = new Sequelize({
-  host: "mysql.railway.internal",
-  dialect: "mysql",
-  username: "root",
-  password: "jndESCgNMxeIQtlQNyiiPRqwUCuZLqeJ",
-  database: "railway",
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   benchmark: true,
   dialectModule: require("mysql2"),
   logging: false,
 });
- 
+
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log("Database connection has been established successfully.");
-    await sequelize.sync({ alter: true });
+    console.log("✅ Database connection has been established successfully.");
+    await sequelize.sync({ alter: true }); // auto update tables if changed
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error("❌ Unable to connect to the database:", error);
   }
 })();
 
